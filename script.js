9952ede638255ad851918e1cf590857037b512e6
RiadB11 <bozhlaniriad443@gmail.com>
2023-12-10 19:01:15 +0100

    fixed some things

--- script.js
+++
@@ -1,5 +0,0 @@
-function validateRegistration() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirm_password = document.getElementById("confirm_password").value;
